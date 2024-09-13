940ac14901f6f3def56b791248e4ff984c45fe29
Jean F Flores <jeeanfrnnds@gmail.com>
2024-09-13 14:13:41 -0400

    Criação da tela inicial

---
+++ resources/views/telaInicial.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DouraSoft - Jean Fernandes F</title>
+
+    </head>
+<body>
+    <header>
+        <h1>Desafio de Banco de Dados</h1>
+    </header>
+    <div class="container">
+        <section>
+            <h2>Sistema de Relatorios</h2>
+            <p>testes testes testestestes testes testestestes testes testestestes testes testes</p>
+        </section>
+        <section>
+            <p>Jean Fernandes</p>
+        </section>
+    </div>
+</body>
+
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+        header {
+            background: #333;
+            color: #fff;
+            padding: 1rem 0;
+            text-align: center;
+        }
+        
+        section {
+            padding: 2rem;
+            background: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+        
+    </style>
+</html>
